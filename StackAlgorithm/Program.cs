09e4a63d63377e9526ae765436ac1ce556ceeb57
Nanang Puji <nanangpuji762@gmail.com>
2022-12-05 16:19:27 +0700

    Add project files.

---
+++ StackAlgorithm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAlgorithm
+{
+    internal class Program
+    { 
+        class Node
+    {
+        /*creates Node for the cicular nexted list*/
+        public int rollNumber;
+        public string nama;
+        public Node next;
+        }
+    static void Main(string[] args)
+        {
+        }
+    }
+}
