8b4a8cf0b24632f3a2eba6a0128016b2165540da
Nanang Puji <nanangpuji762@gmail.com>
2022-12-05 16:23:31 +0700

    add class node

--- StackAlgorithm/Program.cs
+++ StackAlgorithm/Program.cs
@@ -8,16 +8,26 @@ using System.Xml.Linq;
 namespace StackAlgorithm
 {
     internal class Program
-    { 
-        class Node
     {
-        /*creates Node for the cicular nexted list*/
-        public int rollNumber;
-        public string nama;
-        public Node next;
-        }
-    static void Main(string[] args)
+        class Node
         {
+            /*creates Node for the cicular nexted list*/
+            public int rollNumber;
+            public string nama;
+            public Node next;
+        }
+
+        class CircularList
+        {
+            Node LAST;
+            public CircularList()
+            {
+                LAST = null;
+            }
+            static void Main(string[] args)
+            {
+            }
         }
     }
 }
+ 
